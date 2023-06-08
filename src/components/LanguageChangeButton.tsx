7620f048d05889af27bf468953df653c77877dae
benjaminlesieux <benjamin.lesieux@efrei.net>
2023-06-08 14:26:51 +0200

    added react-intl + LanguageChangeButton.tsx as well as an atomic store to change the language

---
+++ src/components/LanguageChangeButton.tsx
@@ -0,0 +1,37 @@
+import { FormattedMessage } from "react-intl";
+import { useAtom } from "jotai";
+import { languageAtom } from "dotenv/atoms/atoms";
+
+const languagesFlags = {
+  fr: "🇫🇷",
+  en: "🇬🇧",
+  //de: "🇩🇪",
+};
+
+function LanguageChangeButton() {
+  const [currentLanguage, setCurrentLanguage] = useAtom(languageAtom);
+
+  return (
+    <>
+      <label htmlFor="language" className="block text-sm font-medium text-gray-900">
+        <FormattedMessage id={"forms.labels.inputs.languages"} />
+      </label>
+
+      <select
+        onChange={e => setCurrentLanguage(e.target.value)}
+        value={currentLanguage}
+        name="languageSelector"
+        id="language"
+        className="mt-1.5 w-full rounded-lg border-gray-300 text-gray-700 sm:text-sm"
+      >
+        {Object.entries(languagesFlags).map(([key, value]) => (
+          <option key={key} value={key}>
+            {value}
+          </option>
+        ))}
+      </select>
+    </>
+  );
+}
+
+export default LanguageChangeButton;
